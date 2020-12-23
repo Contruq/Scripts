8c5c23e2c4635eeaf85608b74caac573d9c367b6
Contruq <59485720+Contruq@users.noreply.github.com>
2020-12-23 21:37:01 +0100

    Add files via upload
    Part 2 of XMAS Auto-Farm
    Released on V3rmillion
    Can be made to    ONE PART ONLY
    (By removing -- on the    false/true  selection)

---
+++ MH-XMAS.lua
@@ -0,0 +1,55 @@
+
+-- Only Keep one Option   on (by changing false to true) else it will break.
+
+-- _G.GiftExchange = false -- false/true 
+-- _G.PresentTeleporter = false -- false/true 
+-- ^^ above is  for a different Part of Script. ^^ --
+
+-- Don't change anything if you don't know --
+
+_G.CFrameSave = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame
+print(_G.CFrameSave)
+
+
+
+function PresentTP()
+    for i,v in pairs(game.Workspace:GetChildren()) do 
+        if v.Name == 'Present' then
+            game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = v.CFrame
+    end
+    end
+end
+
+function GiftExProc()
+    local virtualUser = game:GetService('VirtualUser')
+    virtualUser:CaptureController()
+    virtualUser:SetKeyDown(122)
+    virtualUser:SetKeyUp(122)
+    wait(3)
+    virtualUser:SetKeyDown(122)
+    virtualUser:SetKeyUp(122)
+    wait()
+    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(Vector3.new(-60, 160, 557))
+    wait()
+    local Event = game:GetService("ReplicatedStorage").CashInGift
+    Event:InvokeServer()
+    local Event = game:GetService("ReplicatedStorage").CashInGift
+    Event:InvokeServer()
+    local Event = game:GetService("ReplicatedStorage").CashInGift
+    Event:InvokeServer()
+    local Event = game:GetService("ReplicatedStorage").CashInGift
+    Event:InvokeServer()
+    wait(1)
+    game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.CFrame = _G.CFrameSave
+    
+end
+
+while _G.GiftExchange == true do
+    GiftExProc()
+    wait(15)
+end
+
+while _G.PresentTeleporter == true do
+    PresentTP()
+    wait(15)
+end
